cbde0af27c4ab9d849a908b8013f4bfebbba24f1
falcaomm <manu.m.falcao@gmail.com>
2022-11-27 21:57:57 -0300

    cria e estiliza menu

---
+++ js/menu.js
@@ -0,0 +1,25 @@
+const menuBtn = document.getElementById('menu-btn');
+const nav = document.getElementById('nav-menu');
+
+
+
+function openMenu(event) {
+    if (event.type === 'touchstart') {
+        event.preventDefault()
+    }
+    nav.classList.toggle('active');
+    
+    const active = nav.classList.contains('active')
+    
+    event.currentTarget.setAttribute("aria-expanded", active)
+
+    if (active) {
+        event.currentTarget.setAttribute("aria-label", "Fechar Menu")
+    } else {
+        event.currentTarget.setAttribute("aria-label", "Abrir Menu")
+    }
+}
+
+menuBtn.addEventListener('click', openMenu)
+menuBtn.addEventListener('touchstart', openMenu)
+
