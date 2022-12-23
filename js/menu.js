8b736b916101a4678b9aa908ff576c11a568f95f
falcaomm <manu.m.falcao@gmail.com>
2022-12-23 16:40:46 -0300

    refatora cores e detalhes css

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,13 @@
 const menuBtn = document.getElementById('menu-btn');
 const nav = document.getElementById('nav-menu');
+const header = document.getElementById('header')
 
 function openMenu(event) {
     if (event.type === 'touchstart') {
         event.preventDefault()
     }
     nav.classList.toggle('active');
+    header.classList.toggle('header')
     
     const active = nav.classList.contains('active')
     
@@ -25,4 +27,8 @@ const menuLinks = document.querySelectorAll('.menu-list a[href^="#"]')
 
 menuLinks.forEach((link) => {
     link.addEventListener("click", () => nav.classList.toggle('active'))
+});
+
+menuLinks.forEach((link) => {
+    link.addEventListener("click", () => header.classList.toggle('header'))
 });
